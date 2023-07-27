0dcc8f8240ef55250e2412aab648d2b95624ddff
Sai Ganesh <105867763+saigani219@users.noreply.github.com>
2023-07-27 21:29:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const day = currentDate.getDate();
     const month = currentDate.getMonth() + 1; // Months are zero-indexed, so we add 1
     const year = currentDate.getFullYear();
 
-    const dateString = `${month.toString().padStart(2, ' ')}/${day.toString().padStart(2, '0')}/${year}`;
+    const dateString = `${month.toString().padStart(2, ' ')}/${day.toString().padStart(2, ' ')}/${year}`;
 
     const timeString = currentDate.toLocaleTimeString();
 
