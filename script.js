c64b4cfd0a03ab91ac0c13b94d38c3c903655a9f
Sai Ganesh <105867763+saigani219@users.noreply.github.com>
2023-07-27 21:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function displayDateTime() {
+    const datetimeElement = document.getElementById('timer');
+    const currentDate = new Date();
+const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1; // Months are zero-indexed, so we add 1
+    const year = currentDate.getFullYear();
+
+    const dateString = `${month.toString().padStart(2, ' ')}/${day.toString().padStart(2, '0')}/${year}`;
+
+    const timeString = currentDate.toLocaleTimeString();
+
+    datetimeElement.textContent = dateString + ', ' + timeString;
+  }
+
+  // Update the time and date every second
+  setInterval(displayDateTime, 1000);
